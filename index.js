03475559e60447e27a9fc763f6782843d218da7f
Pranto Deb <rajprit0m@outlook.com>
2025-01-20 19:08:47 +0600

    built-in-module

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const { add, a } = require("./local-1");
 const { add: add2, a: a2 } = require("./local-2");
 console.log(add(2, 3), a);
 console.log(add2(2, 3, 4), a2);
+
+
+// built-in-module
+const path = require('path');
+console.log(path.dirname("E:/Coding/Lavel 2/nodejs-basic/index.js"))
+console.log(path.parse("E:/Coding/Lavel 2/nodejs-basic/index.js"))
+console.log(path.join("E:/Coding/Lavel 2/nodejs-basic/", "local-2.js"))
